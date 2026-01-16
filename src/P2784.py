39c791c011f945cffc31c1a887ed48e33f4526ae
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2026-01-16 10:13:50 +0900

    Solved

---
+++ src/P2784.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        nums.sort()
+        n: int = len(nums)
+        if nums[-1] != n - 1:
+            return False
+        for i in range(n - 1):
+            if nums[i] != i + 1:
+                return False
+        return True
---
+++ tests/test_P2784.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+import os
+
+curr_dir = os.path.dirname(__file__)
+src_dir = os.path.abspath(os.path.join(curr_dir, "..", "src"))
+sys.path.append(src_dir)
+
+from P2784 import Solution
+
+class TestP2784(unittest.TestCase):
+
+  def setUp(self):
+    self.solution = Solution()
+  
+  def test1(self):
+    self.assertEqual(self.solution.isGood([1, 2, 3, 4, 5, 5]), True)
+
+if __name__ == "__main__":
+  unittest.main()
