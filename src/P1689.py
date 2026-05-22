f29650f33a7403a6c34adb4e00f73a83e86449e9
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2026-05-23 07:30:41 +0900

    Solved

---
+++ src/P1689.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        # max_val = 0
+        # for c in n:
+        #     max_val = max(max_val, int(c))
+        # return max_val
+        return int(max(n))
